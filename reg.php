e7c5347990165bfb4b61cc45238426b6acae8595
mariroy <ramroy@mail.ru>
2022-02-13 20:06:11 +0300

    test1

---
+++ reg.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <form action="reg.php" method="POST">
+            <label for="login">Логин</label>
+            <input type="text" id="login">
+            <label for="password">Пароль</label>
+            <input type="password" id="password">
+            <input type="submit">
+        </form>
+    </main>
+</body>
+</html>

1d92acd2b3e56ee8a7c5bc6307ec67f6f802fc95
mariroy <ramroy@mail.ru>
2022-02-16 20:44:51 +0300

    test 2

--- auth.php
+++ auth.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +9,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<?php
+    require("bd.php");
+    $l = "";
+    if  (isset($_REQUEST['login']) && isset($_REQUEST['password']))
+    {
+        $stmt = $db->prepare("SELECT * FROM `users` WHERE `login` = ?");
+        $stmt->execute([$_REQUEST['login']]);
+        $category = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($category != null)
+        {
+            $_SESSION['login']=$_REQUEST['login'];
+            $_SESSION['password']=$_REQUEST['password'];
+            header("Location: main.php");
+        }
+        else
+        {
+            $l = "Invalid login or password";
+        }
+    }
+?>
 <body>
-    
+    <main>
+        <form action="auth.php" method="POST">
+            <label for="login">Логин</label>
+            <input type="text" id="login" name="login">
+            <label for="password">Пароль</label>
+            <input type="password" id="password" name="password">
+            <input type="submit" value="Вход">
+            <a href="reg.php">Зарегистрироваться</a>
+            <?php
+                print("<p>".$l."</p>");
+            ?>
+        </form>
+    </main>
 </body>
 </html>
--- main.php
+++ main.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +9,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<?php
+    if (isset($_SESSION['login']) && isset($_SESSION['password']))
+    {
+
+    }
+    if (isset($_REQUEST['Exit']))
+    {
+        session_destroy();
+        header("Location: auth.php");
+    }
+?>
 <body>
-    
+    <form action="main.php" method="POST">
+        <input type="submit" name = "Exit" value="Exit" style="position: relative; left: 90%;">
+    </form>
 </body>
 </html>
